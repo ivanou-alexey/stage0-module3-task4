86d552ff86f1adaa1c325203e308472246df736f
Alexey Ivanov <ai5973861@gmail.com>
2023-05-12 00:49:13 +0300

    Fix

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int a = 99;
+        char b = (char) a;
+        System.out.print(b);
+        int a1 = 104;
+        char b1 = (char) a1;
+        System.out.print(b1);
+        int a2 = 97;
+        char b2 = (char) a2;
+        System.out.print(b2);
+        int a3 = 114;
+        char b3 = (char) a3;
+        System.out.print(b3);
     }
 }
